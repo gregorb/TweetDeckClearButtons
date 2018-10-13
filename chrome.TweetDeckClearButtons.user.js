2ef871477363519cf19c0ceb8e470b3718db9963
gregorb <github@brecko.si>
2018-10-13 18:27:28 +0200

    Updated & posted to greasyfork.org

--- chrome.TweetDeckClearButtons.user.js
+++ chrome.TweetDeckClearButtons.user.js
@@ -5,6 +5,7 @@
 // @match          https://tweetdeck.twitter.com/*
 // @version        1.0
 // @source         https://github.com/gregorb/TweetDeckClearButtons
+// @namespace https://greasyfork.org/users/219214
 // ==/UserScript==
 
 // Chrome installation instructions: open settings/extensions page, drag&drop this file into it.
@@ -17,37 +18,53 @@ const timer = window.setInterval( function() {
 	if (debug) console.log("GB's TweetDeck Helper: Checking.");
 
 	// list all columns that don't have our "Clear" button yet
-	const columnsToUpdate = document.querySelectorAll("header > a.column-settings-link:not(.gb)");
+	const columnsToUpdate = document.querySelectorAll("div.column-header-links > a:not(.gb)");
 	if (columnsToUpdate && columnsToUpdate.length) {
 
-		if (debug) console.log("GB's TweetDeck Helper: " + JSON.stringify(columnsToUpdate) );
+		if (debug) console.log("GB's TweetDeck Helper: " + columnsToUpdate.length );
 
 		columnsToUpdate.forEach(function(value) {
 			// value = options button
-				
+
+            // tag it, so we know we've already been here
 			value.className += " gb";
 
+            // an extra button may cause the original one to word-wrap out of view; this prevents that from happening
+            value.parentNode.style.whiteSpace = 'nowrap';
+
 			const a = document.createElement("a");
-			a.className += "js-action-header-button column-header-link";
+			a.className = "js-action-header-button column-header-link gb";
 			a.href = "#";
 			a.innerHTML = '<i class="icon icon-clear-timeline"></i>';
 			a.setAttribute("data-action", "clear");
 			a.style.left = "200px";
 			value.parentNode.insertBefore(a, value);
 
-			a.addEventListener("click", function(){ 
+			a.addEventListener("click", function(){
 				value.click();   // open settings panel
 				window.setTimeout(function(){
-	
-					const clrButton = value.parentNode.parentNode.querySelector("button[data-action='clear']");
 
-					if (debug) console.log("GB's TweetDeck Helper: " + JSON.stringify( clrButton ));
+                    // find the root element of our column
+					let clrButton = value;
+                    while ( clrButton && !clrButton.classList.contains('column-holder') )
+                        clrButton = clrButton.parentNode;
+                    if (!clrButton) {
+                        console.log("GB's TweetDeck Helper: column't root element not recognised; class-name changed?");
+                        return;
+                    }
 
-					clrButton.click();   // click on original "clear" button 						
+                    // now find our column's "clear" button
+                    clrButton = clrButton.querySelector("button[data-action='clear']");
+                    if (!clrButton) {
+                        console.log("GB's TweetDeck Helper: column't 'Clear' button not found; class-name changed?");
+                        return;
+                    }
+
+					clrButton.click();   // click on original "clear" button
 					value.click();  // close settings panel
 
 				}, 1000);
-				
+
 			});
 
 		});
@@ -55,5 +72,3 @@ const timer = window.setInterval( function() {
 	}
 
 }, 3000 );
-
-
